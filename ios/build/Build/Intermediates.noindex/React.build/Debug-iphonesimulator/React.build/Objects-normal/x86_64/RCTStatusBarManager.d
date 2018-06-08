a6c27adfeea57c9cbca182246d80f32874a25cfb
Alexandr Voroncov <defauthack@gmail.com>
2018-06-08 15:41:02 +0300

    Refactor code

---
+++ ios/build/Build/Intermediates.noindex/React.build/Debug-iphonesimulator/React.build/Objects-normal/x86_64/RCTStatusBarManager.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/voroncov/Desktop/ale-iphone/node_modules/react-native/React/Modules/RCTStatusBarManager.m \
+  /Users/voroncov/Desktop/ale-iphone/node_modules/react-native/React/Modules/RCTStatusBarManager.h
