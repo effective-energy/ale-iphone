804a8a8ffffe1edbfdfcf43cb4518a88c8f3c39a
Alexandr Voroncov <defauthack@gmail.com>
2018-06-26 18:25:45 +0300

    Refactor recovery phrase screen

--- components/RecoveryPhraseScreen.js
+++ components/RecoveryPhraseScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Button, StyleSheet, StatusBar, TouchableOpacity, Text, Dimensions, Switch, Alert } from 'react-native';
+import { View, StyleSheet, StatusBar, TouchableOpacity, Text, Dimensions, Alert } from 'react-native';
+import CheckBox from 'react-native-check-box';
 
 function wp (percentage) {
     const value = (percentage * viewportWidth) / 100;
@@ -12,10 +13,11 @@ export default class RecoveryPhraseScreen extends React.Component {
     constructor(props) {
         super(props);
 	    this.state = {
-	    	isNobodyLooks: false
+	    	isChecked: false
 	    };
 
 	    this.generateMnemonicPhrase = this.generateMnemonicPhrase.bind(this);
+        this.toggleCheckBox = this.toggleCheckBox.bind(this);
     }
     
     static navigationOptions = {
@@ -23,7 +25,15 @@ export default class RecoveryPhraseScreen extends React.Component {
     };
 
     generateMnemonicPhrase() {
-        
+        if (this.state.isChecked === false) {
+            return Alert.alert('Press on checkbox');
+        } else {
+            return Alert.alert('Next..')
+        }
+    }
+
+    toggleCheckBox() {
+        this.setState({isChecked: !this.state.isChecked})
     }
 
     render() {
@@ -31,14 +41,20 @@ export default class RecoveryPhraseScreen extends React.Component {
             <View style={styles.pageContainer}>
                 <StatusBar barStyle='dark-content' />
                 <View>
-                    <Text style={{ textAlign: 'center' }}>
+                    <Text style={styles.mnemonicGenerateDescription}>
                     	On the following screen, you will see a set of X random words. This is your wallet backup phrase. It can be entered in any version of ALE application in order to back up or restore your wallet’s funds and private key.
                     </Text>
+                    <CheckBox
+                        onClick={() => this.toggleCheckBox}
+                        style={styles.checkBox}
+                        isChecked={this.state.isChecked}
+                        rightText="Make sure nobody looks into your screen unless you want them to have access to your funds."
+                    />
                     <TouchableOpacity
                         onPress={this.generateMnemonicPhrase}
-                        style={{ backgroundColor: '#d1d8dd', padding: 5, borderRadius: 5, display: 'flex', alignItems: 'center', marginTop: 20, padding: 15 }}
+                        style={styles.nextBtn}
                     >
-                        <Text style={{ color: '#000000', fontSize: 18 }}>Continue</Text>
+                        <Text style={styles.nextBtnText}>Continue</Text>
                     </TouchableOpacity>
                 </View>
             </View>
@@ -52,7 +68,27 @@ const styles = StyleSheet.create({
         backgroundColor: '#e8ebee',
         alignItems: 'center',
         width: wp(100),
-        padding: 20,
-
+        padding: 20
+    },
+    mnemonicGenerateDescription: {
+        textAlign: 'center'
+    },
+    checkBox: {
+        marginTop: 20,
+        display: 'flex',
+        alignItems: 'center'
+    },
+    nextBtn: {
+        backgroundColor: '#d1d8dd',
+        padding: 5,
+        borderRadius: 5,
+        display: 'flex',
+        alignItems: 'center',
+        marginTop: 20,
+        padding: 15
+    },
+    nextBtnText: {
+        color: '#000000',
+        fontSize: 18
     }
 });
