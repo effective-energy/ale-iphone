bb2ed5fe8d437b8ed6f1f686dc47499a4c57ec5f
Alexandr Voroncov <defauthack@gmail.com>
2018-07-06 17:44:14 +0300

    Add QRCode Scanner

--- src/components/CameraScreen.js
+++ src/components/CameraScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Linking } from 'react-native';
 
-import { RNCamera } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class CameraScreen extends React.Component {
     constructor(props) {
@@ -13,13 +13,11 @@ export default class CameraScreen extends React.Component {
         header: null,
     };
 
-    takePicture = async function() {
-        if (this.camera) {
-            const options = { quality: 0.5, base64: true };
-            const data = await this.camera.takePictureAsync(options)
-            console.log(data.uri);
-        }
-    };
+    onSuccess(e) {
+        Linking
+        .openURL(e.data)
+        .catch(err => console.error('An error occured', err));
+    }
 
     backToSend() {
         this.props.navigation.push('SendMoney');
@@ -27,52 +25,39 @@ export default class CameraScreen extends React.Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <RNCamera
-                    ref={ref => { this.camera = ref; }}
-                    style = {styles.preview}
-                    type={RNCamera.Constants.Type.back}
-                    flashMode={RNCamera.Constants.FlashMode.on}
-                    permissionDialogTitle={'Permission to use camera'}
-                    permissionDialogMessage={'We need your permission to use your camera phone'}
-                />
-                <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
-                    <TouchableOpacity
-                        onPress={this.takePicture.bind(this)}
-                        style = {styles.capture}
-                    >
-                        <Text style={{fontSize: 14}}> SNAP </Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                        onPress={this.backToSend.bind(this)}
-                        style = {styles.capture}
-                    >
-                        <Text style={{fontSize: 14}}>Back</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            <QRCodeScanner
+                onRead={this.onSuccess.bind(this)}
+                topContent={
+                  <Text style={styles.centerText}>
+                    Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+                  </Text>
+                }
+                bottomContent={
+                  <TouchableOpacity style={styles.buttonTouchable}>
+                    <Text style={styles.buttonText}>OK. Got it!</Text>
+                  </TouchableOpacity>
+                }
+              />
         );
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        backgroundColor: 'black'
-    },
-    preview: {
-        flex: 1,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    capture: {
-        flex: 0,
-        backgroundColor: '#fff',
-        borderRadius: 5,
-        padding: 15,
-        paddingHorizontal: 20,
-        alignSelf: 'center',
-        margin: 20
-    }
+    centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
 });
